3596cfb46aeb2e378ea8154a83655b1262d5dbd1
Anna Shabalina <ashabalina91@gmail.com>
2016-10-13 14:45:16 +0300

    separate files created for NarrowingCasting and NarrowingCastingRunner

---
+++ src/main/java/com/annashabalina/app/classwork/lesson3/NarrowingCastingRunner.java
@@ -0,0 +1,11 @@
+package com.annashabalina.app.classwork.lesson3;
+
+/**
+ * Created by Пользователь on 13.10.2016.
+ */
+public class NarrowingCastingRunner {
+    public static void main(String[] args) {
+        NarrowingCasting casting = new NarrowingCasting();
+        casting.narrowing();
+    }
+}
--- src/main/java/com/annashabalina/app/classwork/lesson3/WideningCasting.java
+++ src/main/java/com/annashabalina/app/classwork/lesson3/WideningCasting.java
@@ -17,20 +17,3 @@ public class WideningCasting {
         System.out.println(double1);
     }
 }
-
-class NarrowingCasting{
-    int int1=0;
-    float float1=3.14159f;
-
-    public void narrowing(){
-        int1=(int) float1;
-     System.out.println(int1);
-    }
-}
-
-class NarrowingCastingRunner{
-    public static void main(String[] args) {
-        NarrowingCasting casting = new NarrowingCasting();
-        casting.narrowing();
-    }
-}
